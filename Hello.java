f209ba89fc9c6441189b79668a8d2d9d94d4d77e
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-02-25 06:20:00 +0545

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static String DoSomething(String message){
+        return message;
+    }
+
+    public String DoSomethingElse(String message){
+        return message ;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        Hello.DoSomething("hi Youtube");
+        Hello hello1 = new Hello();
+        hello1.DoSomethingElse("hi Youtube");
+        System.out.println("Hello world!");
+
+
+    }
+}
